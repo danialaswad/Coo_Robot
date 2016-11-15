66ddf9583d5a29ba31c164a3ba1815a457da93be
Maxime CARLIER <maxime.mca@gmail.com>
2016-11-15 14:51:24 +0100

    Add EtatRobot virtual definition

--- src/EtatRobot.h
+++ src/EtatRobot.h
@@ -6,12 +6,31 @@
 #define COO_ROBOT_ETATROBOT_H
 
 
+#include <string>
+#include "Objet.h"
+#include "Plot.h"
+
 class EtatRobot {
-
 public:
-    // constructor
-    EtatRobot(){};
+    virtual EtatRobot avancer(int x, int y)=0;
 
+    virtual EtatRobot touner(std::string new_diection)=0;
+
+    virtual EtatRobot saisir(Objet o)=0;
+
+    virtual EtatRobot poser()=0;
+
+    virtual EtatRobot peser()=0;
+
+    virtual EtatRobot rencontrerPlot(Plot p)=0;
+
+    virtual EtatRobot evaluerPlot()=0;
+
+    virtual EtatRobot figer()=0;
+
+    virtual EtatRobot repartir()=0;
+
+    virtual EtatRobot afficher()=0;
 };
 
 
